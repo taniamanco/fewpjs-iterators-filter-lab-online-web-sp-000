e3f760139b9dd3508ba9a69e26320769af12e543
Tania Manco <belalov.tania16@gmail.com>
2019-12-04 15:36:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function driversWithRevenueOver(driverArr, revenue) {
+    return driverArr.filter(driver => driver.revenue > revenue);
+}
+
+function driverNamesWithRevenueOver(driverArr, revenue) {
+    return driversWithRevenueOver(driverArr, revenue).map(driver => driver.name);
+}
+
+function exactMatch(driverArr, query) {
+    return driverArr.filter(driver => {
+        let hasMatch = false;
+        for (const key in query) {
+            hasMatch = driver[key] === query[key];
+        }
+        return hasMatch;
+    })
+}
+
+function exactMatchToList(driverArr, query){
+    return exactMatch(driverArr, query).map(driver => driver.name)
+}
